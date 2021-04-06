e08fad9bcc7cd720facce77967d4ab58efb3f9e3
Draga2013 <draga_sorin@yahoo.com>
2021-04-06 14:28:27 +0300

    Task1 : static vs non static, final

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+F
+FundamentalsCoding.iml,3\2\3221ebc3e265cc28126b9dfe49b97396f2e42220
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+T
+$src/main/java/fundamentals/Main.java,a\1\a1eaf701c5efd3ac7c462746b484007a7a691fd5
--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -2,7 +2,21 @@ package fundamentals;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
-        System.out.println("hhhhs");
+//        static vs non static fields and methods
+        TasksExerc tasksExerc1 = new TasksExerc();
+        TasksExerc tasksExerc2 = new TasksExerc();
+        System.out.println("x = " + tasksExerc1.x);
+        System.out.println("y = " + tasksExerc1.y);
+        System.out.println("y = " + tasksExerc2.y);
+        tasksExerc1.x = 22;
+        TasksExerc.y = 3;
+        System.out.println("x1 = " + tasksExerc1.x);
+        System.out.println("x2 = " + tasksExerc2.x);
+        System.out.println("y1 = " + tasksExerc1.y);
+        System.out.println("y2 = " + tasksExerc2.y);
+
+//        task1
+
+        tasksExerc1.circleParametre();
     }
 }

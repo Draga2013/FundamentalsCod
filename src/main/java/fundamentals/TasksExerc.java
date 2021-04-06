d0338bc45e32bf83251f7fa375164313ca882863
Draga2013 <draga_sorin@yahoo.com>
2021-04-06 15:13:09 +0300

    Task1 : static vs non static, final

--- src/main/java/fundamentals/TasksExerc.java
+++ src/main/java/fundamentals/TasksExerc.java
@@ -8,7 +8,7 @@ public class TasksExerc {
     static int y = 1;
 //    Write an application that will read diameter of a circle (variable of type float)
 //    and calculate perimeter of given circle. Firstly, assume π = 3.14. Later,
-//    use value of π from built-in Math class oo
+//    use value of π from built-in Math class
 //    The perimeter is
 
     public static void circleParametre(){
